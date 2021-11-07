bea7c8908ab4e62be6149dd360c98a5a45a33b2b
An_kush <ankush.be.1401@gmail.com>
2021-11-07 01:12:30 +0000

    Added port

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ module.exports = {
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASS,
     DB: process.env.DB_NAME,
+    PORT: process.env.DB_PORT,
     dialect: "mysql",
     pool: {
         max: 5,
