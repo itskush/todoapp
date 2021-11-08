33586fcd7ff50255448bb2a1e24084205488c75a
An_kush <ankush.be.1401@gmail.com>
2021-11-08 13:13:55 +0000

    Added base url

--- front/babel.config.js
+++ front/babel.config.js
@@ -1,3 +1,6 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: '../.env', });
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
--- front/src/services/dataService.js
+++ front/src/services/dataService.js
@@ -1,72 +1,73 @@
 import axios from "axios"
-//https://lofi-tune.herokuapp.com/
+axios.defaults.baseURL = process.env.VUE_APP_APIURL ? process.env.VUE_APP_APIURL : 'http://localhost:3000/';
+
 export default {
     async getTasks() {
         try {
-            return await axios.get("http://localhost:3000/api/tasks/");
+            return await axios.get("api/tasks/");
         } catch (error) {
             return error.response;
         }
     },
     async createTask(userId,description) {
         try {
-            return await axios.post("http://localhost:3000/api/tasks/" + userId, description);
+            return await axios.post("api/tasks/" + userId, description);
         } catch (error) {
             return error.response;
         }
     },
     async updateTask(taskId , description) {
         try {
-            return await axios.put("http://localhost:3000/api/tasks/" + taskId, description);
+            return await axios.put("api/tasks/" + taskId, description);
         } catch (error) {
             return error.response;
         }
     },
     async updateTaskState(taskId) {
         try {
-            return await axios.put("http://localhost:3000/api/tasks/task-state/" + taskId);
+            return await axios.put("api/tasks/task-state/" + taskId);
         } catch (error) {
             return error.response;
         }
     },
     async deleteTask(taskId) {
         try {
-            return await axios.delete("http://localhost:3000/api/tasks/" + taskId);
+            return await axios.delete("api/tasks/" + taskId);
         } catch (error) {
             return error.response;
         }
     },
     async getTasksByUserId(userId) {
         try {
-            return await axios.get("http://localhost:3000/api/tasks/user-tasks/" + userId);
+            return await axios.get("api/tasks/user-tasks/" + userId);
         } catch (error) {
             return error.response;
         }
     },
     async getUsers() {
         try {
-            return await axios.get("http://localhost:3000/api/users/");
+            return await axios.get("api/users/");
         } catch (error) {
             return error.response;
         }
     },
     async addUser(username) {
         try {
-            return await axios.post("http://localhost:3000/api/users/" + username);
+            return await axios.post("api/users/" + username);
         } catch (error) {
             return error.response;
         }
     },
     async updateUser(userId, name) {
         try {
-            return await axios.put("http://localhost:3000/api/users/" + userId , name);
+            return await axios.put("api/users/" + userId , name);
         } catch (error) {
             return error.response;
         }
     },
     async deleteUser(userId) {
         try {
-            return await axios.delete("http://localhost:3000/api/users/" + userId);
+            return await axios.delete("api/users/" + userId);
         } catch (error) {
             return error;
         }
